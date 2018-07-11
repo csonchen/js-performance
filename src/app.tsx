52184aba01c8387b4656c595e9b876985be5dbdf
chensheng <chensheng@lizhiweike.com>
2018-07-11 21:46:13 +0800

    bug fix:更换webpack4不支持某些组件的bug

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Header, About} from '../components'
+import {Header, About} from './components'
 
 export const App: React.StatelessComponent<{}> = () => {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -68,15 +68,17 @@ module.exports = {
    port: 3001,
    noInfo: true,
  },
+ optimization: {
+  splitChunks: {
+    chunks: "all"
+  }
+ },
  plugins: [
    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
    new HtmlWebpackPlugin({
      filename: 'index.html', //Name of file in ./dist/
      template: 'index.html', //Name of template in ./src
      hash: true,
-   }),
-   new webpack.optimize.CommonsChunkPlugin({
-    names: ['vendor', 'manifest'],
    })
  ],
 };
